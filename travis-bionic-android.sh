fa96e3bf94acfa79a37b928e381d6bd1682543c2
Brais Gabín <braisgabin@gmail.com>
2019-11-03 16:12:29 +0100

    asdf

--- travis-bionic-android.sh
+++ travis-bionic-android.sh
@@ -9,7 +9,7 @@ curl -o /tmp/sdk.zip https://dl.google.com/android/repository/sdk-tools-linux-43
 unzip -q -o /tmp/sdk.zip -d ${ANDROID_HOME}
 export PATH="$PATH:$ANDROID_HOME/tools/bin:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools"
 
-# Download dependencies to run sdkmanager with java 11
+# Download dependencies to run sdkmanager on Java 11
 # extracted from: https://stackoverflow.com/a/55982976/842697
 (cd ${ANDROID_HOME}/tools/lib; curl -O http://central.maven.org/maven2/javax/activation/activation/1.1.1/activation-1.1.1.jar)
 (cd ${ANDROID_HOME}/tools/lib; curl -O http://central.maven.org/maven2/javax/xml/jaxb-impl/2.1/jaxb-impl-2.1.jar)
@@ -18,19 +18,17 @@ export PATH="$PATH:$ANDROID_HOME/tools/bin:$ANDROID_HOME/tools:$ANDROID_HOME/pla
 (cd ${ANDROID_HOME}/tools/lib; curl -O http://central.maven.org/maven2/org/glassfish/jaxb/jaxb-jxc/2.3.2/jaxb-jxc-2.3.2.jar)
 (cd ${ANDROID_HOME}/tools/lib; curl -O http://central.maven.org/maven2/javax/xml/bind/jaxb-api/2.3.1/jaxb-api-2.3.1.jar)
 
-cat ${ANDROID_HOME}/tools/bin/sdkmanager
 sed -i -E 's/^CLASSPATH=(.*)$/CLASSPATH=\1:$APP_HOME\/lib\/activation-1.1.1.jar:$APP_HOME\/lib\/jaxb-impl-2.1.jar:$APP_HOME\/lib\/jaxb-xjc-2.3.2.jar:$APP_HOME\/lib\/jaxb-core-2.3.0.1.jar:$APP_HOME\/lib\/jaxb-jxc-2.3.2.jar:$APP_HOME\/lib\/jaxb-api-2.3.1.jar/g' ${ANDROID_HOME}/tools/bin/sdkmanager
-cat ${ANDROID_HOME}/tools/bin/sdkmanager
 
-# Install platform-tools
-sdkmanager "platform-tools"
-
-# Don't show warning: "File $HOME/.android/repositories.cfg could not be loaded."
+# Don't show warning: "File ~/.android/repositories.cfg could not be loaded."
 mkdir -p ~/.android/
 touch ~/.android/repositories.cfg
 
 # Accept the android license
 mkdir -p ${ANDROID_HOME}/licenses
 echo "24333f8a63b6825ea9c5514f83c2829b004d1fee" > "$ANDROID_HOME/licenses/android-sdk-license"
+
+# Install platform-tools
+sdkmanager "platform-tools"
 
 set +xe
