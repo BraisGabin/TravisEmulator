e798250f5cbe1f027633a44bbaaf03925511f23b
Brais Gabín <braisgabin@gmail.com>
2019-11-03 15:27:19 +0100

    asdf

--- travis-bionic-android.sh
+++ travis-bionic-android.sh
@@ -5,6 +5,10 @@ export ANDROID_HOME=$HOME/android-sdk
 mkdir -p ${ANDROID_HOME}
 curl -o /tmp/sdk.zip https://dl.google.com/android/repository/sdk-tools-linux-4333796.zip
 unzip -q -o /tmp/sdk.zip -d ${ANDROID_HOME}
+export PATH="$PATH:$ANDROID_HOME/tools/bin:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools"
+
+# Install platform-tools
+sdkmanager "platform-tools"
 
 # Don't show warning: "File $HOME/.android/repositories.cfg could not be loaded."
 mkdir -p ~/.android/
